a09e6c643e664462bea970f1d089f88fa029526c
monstergog <amn002@ucsd.edu>
2023-07-13 20:22:08 -0700

    added routes for login and profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,4 +8,12 @@ router.get('/', (req, res) => {
   res.render('homepage');
 });
 
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.get('/profile', (req, res) => {
+  res.render('profile');
+});
+
 module.exports = router;
