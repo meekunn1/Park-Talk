8f77e7ae0f4409525b6ce6c305c8219b6ddd2549
Keiji O <meekunn@gmail.com>
2023-07-13 20:48:09 -0700

    added minor comments

--- models/park.js
+++ models/park.js
@@ -18,6 +18,7 @@ Park.init(
         },
     },
     {
+        //freezeTableName: true makes it so table name will not have 's' at the end.
         sequelize,
         timestamps: false,
         freezeTableName: true,
--- models/review.js
+++ models/review.js
@@ -31,6 +31,7 @@ Review.init(
         },
     },
     {
+        //freezeTableName: true makes it so table name will not have 's' at the end.
         sequelize,
         timestamps: false,
         freezeTableName: true,
