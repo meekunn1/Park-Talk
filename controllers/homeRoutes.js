32c9228334aff08a52d4b3243aeb80290101d29e
monstergog <amn002@ucsd.edu>
2023-07-19 20:39:28 -0700

    cookie functionality + logout functionality added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+const withAuth = require('../utils/auth');
 require('dotenv').config();
 
 const router = express.Router();
 
 // Handle the root route
 router.get('/', (req, res) => {
-  res.render('homepage');
+  let logged_in = false;
+
+  if(req.session.logged_in) {
+    logged_in = true;
+  }
+
+  res.render('homepage', { logged_in });
 });
 
 router.get('/login', (req, res) => {
@@ -16,11 +23,17 @@ router.get('/login', (req, res) => {
 router.get('/review', (req, res) => {
   const mapAPI = process.env.MAPS_API;
   const mapLocation = req.query.location;
-  res.render('review', { mapAPI, mapLocation });
+  let logged_in = false;
+
+  if(req.session.logged_in) {
+    logged_in = true;
+  }
+
+  res.render('review', { mapAPI, mapLocation, logged_in });
 });
 
-router.get('/comments', (req, res) => {
-  res.render('comments');
+router.get('/comments', withAuth, (req, res) => {
+  res.render('comments', { logged_in: true });
 });
 
 module.exports = router;
