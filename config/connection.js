7c6f404f5fdf811d68a5de92f3b82e850e377e1e
monstergog <amn002@ucsd.edu>
2023-07-20 08:26:11 -0700

    attempt to fix sequelize connection to heroku

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
+if (process.env.CLEARDB_DATABASE_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
     dialect: 'mysql',
   });
 } else {
